8f82bf29d70b7bcd9f2b53ab908bbe8b48743c86
Tarik Curto <tcurto@opensistemas.com>
2018-03-14 16:11:59 +0100

    + Added morgan logger

--- digester.js
+++ digester.js
@@ -1,9 +1,12 @@
 var express = require('express')
+var morgan = require('morgan')
 var app = express()
+
+app.use(morgan('combined'))
 
 app.get('/', function (req, res) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  res.send("Hellow, i'm digester. You are " + ip + ".");
+  res.send("Hello, i'm digester. You are " + ip + ".");
 });
 
 app.listen(80);
