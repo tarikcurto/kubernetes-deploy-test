d243d423323a4d8f0abb459fa60cb58cadeb18a6
Tarik Curto <tcurto@opensistemas.com>
2018-04-20 14:07:01 +0200

    + Fili request

---
+++ auth.js
@@ -0,0 +1,14 @@
+#auth.js
+
+var express = require('express')
+var morgan = require('morgan')
+var app = express()
+
+app.use(morgan('combined'))
+
+app.get('/', function (req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.send("Hello, i'm auth. You are " + ip + ".");
+});
+
+app.listen(80);
